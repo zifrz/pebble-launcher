5d4eee261675484f15b7827c2f89f6ffef99bdaf
zifrz <114908186+zifrz@users.noreply.github.com>
2024-04-07 10:56:19 +0530

    Removed duplicate code that caused App Drawer to be opened twice

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,67 +23,62 @@ class HomePage extends StatelessWidget {
         //     size: 40,
         //   ),
         // ),
-        body: GestureDetector(
-          onVerticalDragDown: (DragDownDetails details) {
-            Navigator.pushNamed(context, "apps");
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(32.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
+        body: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
 
-                // Column Containing Clock and Favourite Apps
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 48),
-                    DigitalClock(
-                        textScaleFactor: 2,
-                        padding: const EdgeInsets.symmetric(vertical: 16),
-                        showSeconds: false,
-                        isLive: true,
-                        digitalClockTextColor: Colors.white,
-                        // decoration: const BoxDecoration(
-                        //     color: Colors.white,
-                        //     shape: BoxShape.rectangle,
-                        //     borderRadius: BorderRadius.all(Radius.circular(15))),
-                        datetime: DateTime.now()),
-                    const SizedBox(height: 32),
-                    const Text(
-                      "Fav App 1",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "Fav App 2",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "Fav App 3",
-                      style: TextStyle(color: Colors.white, fontSize: 24),
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                ),
-
-                // App Drawer Icons
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, "apps");
-                  },
-                  padding: const EdgeInsets.all(32),
-                  iconSize: 40,
-                  icon: const Icon(
-                    Icons.arrow_upward_rounded,
-                    color: Colors.white,
+              // Column Containing Clock and Favourite Apps
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 48),
+                  DigitalClock(
+                      textScaleFactor: 2,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      showSeconds: false,
+                      isLive: true,
+                      digitalClockTextColor: Colors.white,
+                      // decoration: const BoxDecoration(
+                      //     color: Colors.white,
+                      //     shape: BoxShape.rectangle,
+                      //     borderRadius: BorderRadius.all(Radius.circular(15))),
+                      datetime: DateTime.now()),
+                  const SizedBox(height: 32),
+                  const Text(
+                    "Fav App 1",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
                   ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Fav App 2",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "Fav App 3",
+                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+
+              // App Drawer Icons
+              IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "apps");
+                },
+                padding: const EdgeInsets.all(32),
+                iconSize: 40,
+                icon: const Icon(
+                  Icons.arrow_upward_rounded,
+                  color: Colors.white,
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
