6c041e8afa6fa0018694b45af9565d6c59316cb0
zifrz <114908186+zifrz@users.noreply.github.com>
2024-04-17 16:00:32 +0530

    Installation & Uninstallation of apps reflect in App Drawer (app_install_events)

--- lib/models/appinfo_list_model.dart
+++ lib/models/appinfo_list_model.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
+import 'package:app_install_events/app_install_events.dart';
+// import 'package:app_install_events/';
 
 class AppInfoListModel extends ChangeNotifier {
   List<AppInfo> _appInfoList = [];
 
   List<AppInfo> get appInfoList => _appInfoList;
 
-  AppInfoListModel(){
-    getApplications();
-  }
+  late AppIUEvents _appIUEvents;
 
   void getApplications() async {
     _appInfoList = await InstalledApps.getInstalledApps();
@@ -17,5 +17,24 @@ class AppInfoListModel extends ChangeNotifier {
         .sort((a, b) => a.name.toLowerCase().compareTo(b.name.toLowerCase()));
 
     notifyListeners();
+  }
+
+  AppInfoListModel() {
+    _appIUEvents = AppIUEvents();
+    getApplications();
+
+    _appIUEvents.appEvents.listen((event) {
+      // Check if the event is an install event
+      if (event.type == IUEventType.installed) {
+        print("App installed: ${event.packageName}");
+        getApplications();
+      }
+
+      // Check if the event is an uninstall event
+      if (event.type == IUEventType.uninstalled) {
+        print("App uninstalled: ${event.packageName}");
+        getApplications();
+      }
+    });
   }
 }
